8a38fc8873f81839d4b84ec6fbae2179b8a8f0f2
Ernest Tan <ernest.tanhk@gmail.com>
2024-05-12 18:34:10 +0800

    fixed typing bug

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,67 @@
 from flask import Flask, request
 from openai import OpenAI
+from dotenv import load_dotenv
+from os import environ
+from pinecone import Pinecone
 
 
+load_dotenv()
 app = Flask(__name__)
-app.config.from_pyfile('settings.py')
+
+client = OpenAI(
+    organization=environ.get("OPENAI_ORG"),
+    project=environ.get("OPENAI_PROJ"),
+)
+pc = Pinecone(api_key=environ.get("PINECONE_API_KEY"))
+index = pc.Index("entries-db")
+
+
+@app.route('/put-embeddings', methods=['POST'])
+def put_embeddings():
+    entry = request.form.get('entry')
+
+    embedding = client.embeddings.create(
+        model="text-embedding-ada-002",
+        input=[entry],
+        encoding_format="float"
+    )
+
+    # TODO: update [0] when using multiple entries
+    embedding_vectors = [embedding.data[0].embedding]
+    meta = [entry]
+    to_upsert = zip(0, embedding_vectors, meta)
+
+    print(embedding_vectors)
+    index.upsert(vectors=list(to_upsert))
+
+    return {"status": "ok"}, 200
 
 
 @app.route('/entry-analysis', methods=['POST'])
-def hello():
+def entry_analysis():
     entry = request.form.get('entry')
-
-    client = OpenAI(
-        organization='org-kNwnRBLHkjuC5uYLKFaD9zd2',
-        project='proj_1uDpIUHImYbx2ciPLGumZguw',
-    )
 
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "system", "content": "You are a therapist reading one of your client's journal entry. Based on his mood and activities, recommend him 3 activities to do."},
+            {"role": "user", "content": entry}
+        ]
+    )
+
+    res = (completion.choices[0].message.content)
+
+    return {"response": res}, 200
+
+
+@app.route('/suggest-continuation', methods=['POST'])
+def suggest_continuation():
+    entry = request.form.get('entry')
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a professional journal entry writer. Your friend is having trouble continuing his journal entry. Give 3 actionable suggestions on what he can write about."},
             {"role": "user", "content": entry}
         ]
     )
