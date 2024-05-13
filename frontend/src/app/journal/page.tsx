a5e15e927e9262d90d23aac60be64fdf58c8692a
Ernest Tan <ernest.tanhk@gmail.com>
2024-05-13 08:18:45 +0800

    added loading button screen

--- frontend/src/app/journal/page.tsx
+++ frontend/src/app/journal/page.tsx
@@ -1,11 +1,13 @@
 import JournalHeader from "@/components/JournalHeader";
 import JournalSpace from "@/components/JournalSpace";
+import LoadingScreen from "@/components/LoadingScreen";
 
 export default function Home() {
   return (
-    <div className="h-screen p-6 overflow-scroll bg-gradient-to-b from-violet-950 to-indigo-300 flex flex-col gap-3">
+    <div className="h-screen overflow-scroll bg-gradient-to-b from-violet-950 to-indigo-300 flex flex-col gap-3">
       <JournalHeader />
       <JournalSpace />
+      {/* <LoadingScreen /> */}
     </div>
   );
 }
--- frontend/src/components/JournalHeader.tsx
+++ frontend/src/components/JournalHeader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const JournalHeader = () => {
   return (
-    <div className="pt-10">
+    <div className="pt-10 mt-6 mx-6">
       <h1 className="text-white text-3xl font-bold">Entry</h1>
     </div>
   );
