24f233aa7ed0da7f31180fe966854f0c5658ee96
Ernest Tan <ernest.tanhk@gmail.com>
2024-05-14 08:31:07 +0800

    fixed wrong metadata

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,14 +2,22 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import type { Viewport } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "JournalBoomBoom",
   description: "Your pefect lifecoach in your pocket",
-  viewport:
-    "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  // Also supported by less commonly used
+  // interactiveWidget: 'resizes-visual',
 };
 
 export default function RootLayout({
